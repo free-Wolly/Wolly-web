93606955a9470ac22996f1262c39fe4fd2fada64
Erekle98 <e.lomsadze1@gmail.com>
2023-01-16 19:14:51 +0400

    Fix image bug

--- components/Landing/Blog/Blog.tsx
+++ components/Landing/Blog/Blog.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import { blogItems } from "./constanats";
 import { SidePost } from "./SidePost";
 
@@ -7,11 +8,16 @@ export const Blog = () => {
     <div className="container mx-auto 2xl:px-32 xl:px-16 lg:px-16 md:px-8 sm:px-4 px-4 border-t-2">
       <div className="text-center text-3xl font-bold py-16">Blog</div>
       <div className="w-full flex flex-col lg:flex-row gap-8 mb-16">
-        <div
-          className={`w-full lg:w-3/6 flex flex-col justify-end bg-[url('/assets/images/sliderImages/${blogItems[0].img}')] bg-fill rounded-3xl p-6 pt-64 lg:p-12 lg:pt-0`}
-        >
-          <div className="text-xl font-bold">{blogItems[0].title}</div>
-          <div className="mt-4">{blogItems[0].date}</div>
+        <div className="relative w-full lg:w-3/6 flex flex-col justify-end rounded-3xl p-6 pt-64 lg:p-12 lg:pt-0">
+          <Image
+            className="absolute inset-0 h-full rounded-3xl"
+            src={`/assets/images/sliderImages/${blogItems[0].img}`}
+            alt=""
+            width={1000}
+            height={1000}
+          />
+          <div className="text-xl font-bold z-10">{blogItems[0].title}</div>
+          <div className="mt-4 z-10">{blogItems[0].date}</div>
         </div>
         <div className="lg:w-3/6 flex flex-row gap-8">
           <SidePost blogItem={blogItems[1]} />

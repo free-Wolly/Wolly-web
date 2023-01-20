a28fa9c1dbc9cbd467afddb68f8ad8e5f7d964da
Erekle98 <e.lomsadze1@gmail.com>
2023-01-20 20:47:12 +0400

    Add FAQ section

---
+++ components/Landing/Faq/Question.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { motion } from "framer";
+import { imgVariant, openVariant } from "./constants";
+
+export const Question = ({ question }: any) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <motion.div
+      className={`w-full rounded-3xl lg:w-3/6 p-4 cursor-pointer ${
+        open ? "bg-red-100" : "hover:bg-red-100"
+      }`}
+      onClick={() => setOpen(!open)}
+    >
+      <motion.div className="flex flex-row items-center justify-between rounded-2xl">
+        <motion.div className="text-xl font-bold">
+          {question.question}
+        </motion.div>
+        <motion.img
+          initial="close"
+          animate={open ? "open" : "close"}
+          className="w-4 h-4"
+          src="/assets/images/plus.svg"
+          alt=""
+          variants={imgVariant}
+        />
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        animate={open ? "show" : "hidden"}
+        variants={openVariant}
+        className="pointer-events-none"
+      >
+        {question.answer}
+      </motion.div>
+    </motion.div>
+  );
+};

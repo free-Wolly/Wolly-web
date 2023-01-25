4f25f0e966520c7b612e2870f0d91c43fc17cf35
Erekle98 <e.lomsadze1@gmail.com>
2023-01-25 19:43:08 +0400

    add get the app section

---
+++ components/Landing/GetApp/constants.ts
@@ -0,0 +1,13 @@
+import googleStoreImg from "../../../public/assets/images/google-store.png";
+import appStoreImg from "../../../public/assets/images/app-store.png";
+
+export const storesInfo = [
+  {
+    img: googleStoreImg,
+    link: "https://play.google.com/store/apps/details?id=com.wolly&pli=1",
+  },
+  {
+    img: appStoreImg,
+    link: "https://apps.apple.com/ge/app/wolly/id1546265724",
+  },
+];

1374f670cb949322528ac0c0355b14486e65c36a
Erekle98 <e.lomsadze1@gmail.com>
2023-01-25 14:34:10 +0400

    Add slider for small screens

--- components/Landing/BeforeAfter/constants.ts
+++ components/Landing/BeforeAfter/constants.ts
@@ -37,22 +37,9 @@ export const variants = {
     x: 0,
     opacity: 1,
     scale: 1,
-    // transition: 'ease-in',
     transition: {
       x: { type: "spring", stiffness: 300, damping: 30 },
       opacity: { duration: 0.2 },
     },
-  },
-  exit: (direction: any) => {
-    return {
-      x: direction > 0 ? -1000 : 1000,
-      opacity: 0,
-      scale: 0.5,
-      // transition: 'ease-in',
-      transition: {
-        x: { type: "spring", stiffness: 300, damping: 30 },
-        opacity: { duration: 0.2 },
-      },
-    };
   },
 };

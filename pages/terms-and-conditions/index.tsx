96ce3369e8a00351c6195438b0e3d7d3ad3c6de3
Sandro <schalagashvili@gmail.com>
2023-02-04 12:08:22 +0400

    fix import issue

--- pages/terms-and-conditions/index.tsx
+++ pages/terms-and-conditions/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import React from 'react'
-import Footer from '../../components/Footer'
+import { Footer } from '../../components/Footer/Footer'
 import Header from '../../components/TermsAndConditions/Header'
 import { Terms } from '../../components/TermsAndConditions/Terms'
 

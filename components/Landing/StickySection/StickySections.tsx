5f82b195c87354698213a98f0e05eeba19e8a473
Erekle98 <e.lomsadze1@gmail.com>
2023-02-17 16:53:20 +0400

    Add background to sticky section

--- components/Landing/StickySection/StickySections.tsx
+++ components/Landing/StickySection/StickySections.tsx
@@ -22,7 +22,7 @@ const StickySections = () => {
               : id === 4
               ? "lg:ml-[80%]"
               : ""
-          } ${id === 0 ? "mt-0" : "lg:mt-[500px]"} rounded-2xl mb-2`}
+          } lg:mt-[500px] rounded-2xl mb-2`}
           src={image}
           alt=""
           width={1000}
@@ -32,8 +32,20 @@ const StickySections = () => {
     });
   }, []);
   return (
-    <div className="container mx-auto 2xl:px-32 xl:px-16 lg:px-16 md:px-8 sm:px-4 px-4 border-t-2 py-8 lg:py-32">
-      <div className="w-full relative flex flex-col py-16">{renderImages}</div>
+    <div
+      style={{
+        backgroundImage:
+          'url("/assets/images/black-pink-gradient.png"), url("/assets/images/black-pink-gradient.png")',
+        backgroundSize: "cover, cover",
+        backgroundPosition: "center top, center bottom",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="container mx-auto 2xl:px-32 xl:px-16 lg:px-16 md:px-8 sm:px-4 px-4 py-8 lg:py-32">
+        <div className="w-full relative flex flex-col py-16 ">
+          {renderImages}
+        </div>
+      </div>
     </div>
   );
 };
